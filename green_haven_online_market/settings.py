662844c2821620a3aebf7299fe3b6bb83d39add9
Katalin Molnar <74814196+molnarlin@users.noreply.github.com>
2025-02-10 21:13:39 +0000

    authentication backend added

--- green_haven_online_market/settings.py
+++ green_haven_online_market/settings.py
@@ -143,6 +143,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 SOCIALACCOUNT_PROVIDERS = {}
