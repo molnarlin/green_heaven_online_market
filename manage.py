6af77c26b5771746ccd7e77b6140e6a8da9c11f7
Katalin Molnar <74814196+molnarlin@users.noreply.github.com>
2025-05-13 20:05:00 +0100

    manage.py updated

--- manage.py
+++ manage.py
@@ -6,7 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'green_haven_online_market.settings')
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE',
+        'green_heaven_online_market.settings'
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
