09ca018dad6a3b3f2ca571e83fc1f1c12a2261ed
Katalin Molnar <molnarlin@gmail.com>
2025-05-13 12:35:42 +0100

    name updated

--- green_haven_online_market/wsgi.py
+++ green_haven_online_market/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'green_haven_online_market.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'green_heaven_online_market.settings')
 
 application = get_wsgi_application()
