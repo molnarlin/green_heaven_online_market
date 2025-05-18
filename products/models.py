2de5a73e63e8fc64452f0130d2fbfc9f5892f893
Katalin Molnar <74814196+molnarlin@users.noreply.github.com>
2025-05-18 23:19:56 +0100

    more details added to products

--- products/models.py
+++ products/models.py
@@ -16,6 +16,13 @@ class Category(models.Model):
         return self.friendly_name
 
 
+class CareInfo(models.Model):
+    light = models.CharField(max_length=100)
+    water = models.CharField(max_length=300)
+    temperature = models.CharField(max_length=100)
+    humidity = models.CharField(max_length=100)
+
+
 class Product(models.Model):
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL
@@ -29,6 +36,15 @@ class Product(models.Model):
     )
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    care_info = models.OneToOneField(CareInfo, on_delete=models.CASCADE)
+    fertilizer = models.CharField(max_length=300, null=True, blank=True)
+    pests = models.CharField(max_length=300, null=True, blank=True)
+    pruning = models.CharField(max_length=300, null=True, blank=True)
+    repotting = models.CharField(max_length=300, null=True, blank=True)
+    delivery = models.CharField(max_length=300, null=True, blank=True)
+    planting = models.CharField(max_length=300, null=True, blank=True)
+    harvesting = models.CharField(max_length=300, null=True, blank=True)
+    size = models.CharField(max_length=300, null=True, blank=True)
 
     def __str__(self):
         return self.name
