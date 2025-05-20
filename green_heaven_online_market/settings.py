97b97cb6a37f21d1515028cfedfe796bd4d6e1d6
Katalin Molnar <74814196+molnarlin@users.noreply.github.com>
2025-05-20 20:18:30 +0100

    added shopping bag app urls and template

--- green_heaven_online_market/settings.py
+++ green_heaven_online_market/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'products',
+    'bag',
 ]
 
 MIDDLEWARE = [
--- green_heaven_online_market/urls.py
+++ green_heaven_online_market/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('accounts/profile/', views.profile, name='profile'),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
+    path('bag/', include('bag.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
