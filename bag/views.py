c8299d565591906c1be3f959cf2a302825705d28
Katalin Molnar <74814196+molnarlin@users.noreply.github.com>
2025-06-15 20:43:43 +0100

    remove / updated function in bag updated

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
@@ -12,6 +15,8 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     color = None
@@ -20,15 +25,19 @@ def add_to_bag(request, item_id):
     bag = request.session.get('bag', {})
 
     if color:
-        if item_id in bag:
-            if 'items_by_color' not in bag[item_id]:
-                bag[item_id]['items_by_color'] = {}
-            if color in bag[item_id]['items_by_color']:
+        if item_id in list(bag.keys()):
+            if color in bag[item_id]['items_by_color'].keys():
                 bag[item_id]['items_by_color'][color] += quantity
             else:
                 bag[item_id]['items_by_color'][color] = quantity
         else:
             bag[item_id] = {'items_by_color': {color: quantity}}
+    else:
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -40,52 +49,43 @@ def adjust_bag(request, item_id):
     Handles both color-specific and non-color products.
     """
     quantity = int(request.POST.get('quantity'))
-    color = request.POST.get('product_color', None)
+    color = None
+    if 'product_color' in request.POST:
+        color = request.POST['product_color']
     bag = request.session.get('bag', {})
 
     if color:
-        # Ensure the item and color exist before modifying
-        if item_id in bag:
-            if 'items_by_color' not in bag[item_id]:
-                bag[item_id]['items_by_color'] = {}
-
-            if quantity > 0:
-                bag[item_id]['items_by_color'][color] = quantity
-            else:
-                bag[item_id]['items_by_color'].pop(color, None)
-                if not bag[item_id]['items_by_color']:
-                    bag.pop(item_id)
+        if quantity > 0:
+            bag[item_id]['items_by_color'][color] = quantity
         else:
-            if quantity > 0:
-                bag[item_id] = {'items_by_color': {color: quantity}}
+            del bag[item_id]['items_by_color'][color]
+            if not bag[item_id]['items_by_color']:
+                bag.pop(item_id)
     else:
         if quantity > 0:
             bag[item_id] = quantity
         else:
-            bag.pop(item_id, None)
+            bag.pop(item_id)
 
     request.session['bag'] = bag
-    return redirect('view_bag')
+    return redirect(reverse('view_bag'))
 
 
 def remove_from_bag(request, item_id):
     try:
+        item_id = str(item_id)
         color = request.POST.get('product_color', None)
         bag = request.session.get('bag', {})
 
         if color:
-            if item_id in bag and 'items_by_color' in bag[item_id]:
-                bag[item_id]['items_by_color'].pop(color, None)
-                if not bag[item_id]['items_by_color']:
-                    bag.pop(item_id, None)
+            del bag[item_id]['items_by_color'][color]
+            if not bag[item_id]['items_by_color']:
+                bag.pop(item_id)
         else:
-            bag.pop(item_id, None)
+            bag.pop(item_id)
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
-    except Exception as e:
-        print(
-            f"Error removing item from bag: {e}"
-        )  # Optional: log for debugging
+    except Exception:
         return HttpResponse(status=500)
