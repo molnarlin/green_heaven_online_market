d06f994e955a74c1003d76b215041d78de4edac0
Katalin Molnar <74814196+molnarlin@users.noreply.github.com>
2025-06-16 22:02:04 +0100

    fixing signals

--- checkout/models.py
+++ checkout/models.py
@@ -43,7 +43,7 @@ class Order(models.Model):
         self.order_total = (
             self.lineitems.aggregate(
                 Sum('lineitem_total')
-            )['lineitem_total__sum']
+            )['lineitem_total__sum'] or 0
         )
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = (
