ed2791359f4927e419ae04037ba933d905ad13ec
Katalin Molnar <74814196+molnarlin@users.noreply.github.com>
2025-07-01 14:51:40 +0100

    post_list updated and comments added

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,11 @@ from django.conf import settings
 from django.urls import reverse
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -22,6 +27,8 @@ class Post(models.Model):
         choices=Status.choices,
         default=Status.DRAFT,
     )
+    objects = models.Manager()  # Default manager
+    published = PublishedManager()  # Custom manager for published posts
 
     class Meta:
         ordering = ('-publish',)
@@ -40,5 +47,26 @@ class Post(models.Model):
                 self.publish.month,
                 self.publish.day,
                 self.slug,
-            ],
+            ]
         )
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [models.Index(fields=['created']),]
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post.title}"
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.views.decorators.http import require_POST
+from .forms import CommentForm
 
 
 class PostListView(ListView):
@@ -38,3 +40,23 @@ def post_detail(request, year, month, day, post):
         publish__day=day
     )
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+        return render(
+            request,
+            'blog/comment.html',
+            {
+                'post': post,
+                'form': form,
+                'comment': comment
+            }
+        )
