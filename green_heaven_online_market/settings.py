e91989fa55bd26b7e13c8211f9322eb0ad822d65
Katalin Molnar <74814196+molnarlin@users.noreply.github.com>
2025-07-02 13:35:12 +0100

    sign in and sign up updated

--- green_heaven_online_market/settings.py
+++ green_heaven_online_market/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
@@ -232,8 +231,6 @@ SOCIALACCOUNT_PROVIDERS = {
         'OAUTH_PKCE_ENABLED': True,
     }
 }
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config('GOOGLE_AUTH2_KEY')
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config('GOOGLE_AUTH2_SECRET')
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
