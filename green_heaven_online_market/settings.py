bc815ba88b49d83707eb4335ecf9e48837bd2a83
Katalin Molnar <74814196+molnarlin@users.noreply.github.com>
2025-08-23 18:01:55 +0100

    Add AWS_S3_FILE_OVERWRITE setting to prevent overwriting files in S3

--- green_heaven_online_market/settings.py
+++ green_heaven_online_market/settings.py
@@ -209,6 +209,7 @@ if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'green-heaven-online-garden-centre'
     AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_S3_FILE_OVERWRITE = False
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
